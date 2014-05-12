f0c588a2d9dc1b5481780d2dff899d4b5ecf9ce4
sebalo1314daw2 <sebalo1314daw2@gmail.com>
2014-05-13 00:10:23 +0200

    Web application languages ​​for ONCE 4.1.0
    - finalizado el estilo, los iFrames y la interfaz
    - hacer el registro
    - hacer todo el SQL (voy por el user)

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -8,3 +8,30 @@
 --         surnames VARCHAR(100) 
 --         
 -- ) ENGINE=InnoDB;
+-- Create word table
+CREATE TABLE word
+(
+	id                      INT(6)           PRIMARY KEY        AUTO_INCREMENT      ,
+	value                   VARCHAR(20)      NOT NULL           UNIQUE              ,
+        number_of_syllabes      INT(1)           NOT NULL                               ,
+        category                VARCHAR(10)      NOT NULL                               
+) ENGINE=InnoDB;
+-- Create game table
+CREATE TABLE game
+(
+	id                                  INT(6)           PRIMARY KEY        AUTO_INCREMENT      ,
+	name                                VARCHAR(50)      NOT NULL           UNIQUE              ,
+        short_description                   VARCHAR(100)     NOT NULL                               ,
+        rules                               VARCHAR(200)     NOT NULL           UNIQUE              ,
+        punctuation_at_the_first_attempt    INT(2)           NOT NULL                               ,
+        punctuation_at_the_second_attempt   INT(2)                                                  ,
+        time_of_first_attempt               INT(2)           NOT NULL                               ,
+        time_of_second_attempt              INT(2)                                                  ,
+        num_of_words                        INT(2)           NOT NULL                               
+) ENGINE=InnoDB;
+-- Create game table
+CREATE TABLE game
+(
+	id                                  INT(6)           PRIMARY KEY        AUTO_INCREMENT      ,
+	                        
+) ENGINE=InnoDB;
