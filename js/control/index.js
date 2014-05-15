5ecbb83ac7ac0741dcb9a20b03a8d6cb426238f4
sebalo1314daw2 <sebalo1314daw2@gmail.com>
2014-05-15 16:07:59 +0200

    Web application languages ​​for ONCE 4.3.2
    - finalizado el estilo, los iFrames, la interfaz, el SQL y los selects
    - hacer el registro
    - enlazar con el registro.html, con la pag de la once y que se oculte y se muestre parte del form

---
+++ js/control/index.js
@@ -0,0 +1,11 @@
+$("document").ready(function(){redirectHomePage();});
+/**
+ * redirectHomePage()
+ * @description Procedure that aims to redirect the web to home page.
+ * @author Sergio Baena López
+ * @version 1.0
+ */
+function redirectHomePage()
+{
+    window.location.href = "html/home.html";
+}
